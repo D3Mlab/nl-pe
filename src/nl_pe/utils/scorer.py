9454a07e6a899735227c910791216e0f563ef327
Anton Korikov <anton.korikov@mail.utoronto.ca>
2026-02-21 19:06:39 -0500

    fix query label bug

--- src/nl_pe/utils/scorer.py
+++ src/nl_pe/utils/scorer.py
@@ -20,7 +20,13 @@ class TextScorer(Scorer):
         
         self.cache_path = self.config.get('data',{}).get('cache_path')
         self.texts_csv_path = self.config.get('data',{}).get('d_text_csv')
-        self.query_rel_label = float(self.config.get('gp',{}).get('query_rel_label'))
+        
+        query_rel_label = self.config.get('gp', {}).get('query_rel_label')
+
+        if query_rel_label is not None:
+            self.query_rel_label = float(query_rel_label)
+        else:
+            self.query_rel_label = None
 
         # -------------------------
         # Load corpus texts ONCE
