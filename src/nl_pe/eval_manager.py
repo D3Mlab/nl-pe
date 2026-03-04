f74ddf293117cb3c1537f4365759cfa0e2d60505
Anton Korikov <anton.korikov@mail.utoronto.ca>
2026-03-04 09:42:06 -0500

    res

--- src/nl_pe/eval_manager.py
+++ src/nl_pe/eval_manager.py
@@ -267,6 +267,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Evaluate an experiment based on a config file.")
     parser.add_argument("-c", "--eval-dir", type=str, help="The path to the evaluation dir containing eval_config.yaml")
     parser.add_argument("-se", "--skip-existing", action="store_true", help="Skip evaluation if output files already exist.")
+    parser.add_argument("-t", "--times", action="store_true", help="Do timing eval or not")
     args = parser.parse_args()
 
     eval_manager = EvalManager(args.eval_dir, skip_existing=args.skip_existing)
